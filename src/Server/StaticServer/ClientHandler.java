66eeae9ea6b6c3f857da70984238a94301723bfd
loloka99 <anndrass99@gmail.com>
2024-04-29 19:26:06 +0300

    Implementing time measurement for clients.

--- src/Server/StaticServer/ClientHandler.java
+++ src/Server/StaticServer/ClientHandler.java
@@ -1,5 +1,9 @@
 package Server.StaticServer;
 
+import Server.CSVWriter;
+import Server.ElapsedTimes;
+import Server.TimeMeasurement;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,28 +13,38 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ClientHandler implements Runnable {
-    private Socket clientSocket;
-    private int[] array ;
+    private final Socket clientSocket;
+    private final int[] array ;
     private  int result;
+    private final TimeMeasurement timer = new TimeMeasurement();
+    private final ElapsedTimes elapsedTimes;
 
-    public ClientHandler(Socket clientSocket, int[] array) {
+
+    public ClientHandler(Socket clientSocket, int[] array, ElapsedTimes elapsedTimes) {
         this.clientSocket = clientSocket;
         this.array = array;
+        this.elapsedTimes = elapsedTimes;
     }
 
     @Override
     public void run() {
-        try (
+        try  {
+            timer.start();
+            try (
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ){
-            //Converting to string
-            String arrayAsString = Arrays.stream(array)
-                    .mapToObj(String::valueOf)
-                    .collect(Collectors.joining(" "));
-            out.println(arrayAsString);
-            String response = in.readLine();
-            result = Integer.parseInt(response);
+                ) {
+                //Converting to string
+                String arrayAsString = Arrays.stream(array)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(" "));
+                out.println(arrayAsString);
+                String response = in.readLine();
+                result = Integer.parseInt(response);
+                timer.stop();
+                // Add elapsed time to the list
+                elapsedTimes.addClientElapsedTime(timer.getElapsedTime());
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
@@ -38,5 +52,9 @@ public class ClientHandler implements Runnable {
 
     public int getResult() {
         return result;
+    }
+
+    public ElapsedTimes getElapsedTimes() {
+        return elapsedTimes;
     }
 }
