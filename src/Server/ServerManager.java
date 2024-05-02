16efc17bffbf1f057cdf6ef7fb69a311b981c9f5
loloka99 <anndrass99@gmail.com>
2024-05-02 19:01:02 +0300

    Implementing argument handling.

--- src/Server/ServerManager.java
+++ src/Server/ServerManager.java
@@ -7,9 +7,30 @@ import Server.StaticServer.StaticServer;
 
 public class ServerManager {
     public static void main(String[] args) {
-        ServerStrategy server = new DynamicServer();
-        server.run();
-        System.out.println("Total: " + server.getTotalSum() + ". Elapsed time: " + server.getElapsedTime() + "ms");
+        int miniTaskSize = 3;
+        if (args.length == 0) {
+            System.err.println("No arguments provided.");
+            return;
+        } else if (args.length > 2) {
+            System.err.println("Too many arguments.");
+            return;
+        } else if (args.length == 2) {
+            try {
+                miniTaskSize = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid mini task size.");
+                return;
+            }
+        }
+        if (args[0].equals("static")) {
+            StaticServer staticServer = new StaticServer();
+            staticServer.run();
+        } else if (args[0].equals("dynamic")) {
+            DynamicServer dynamicServer = new DynamicServer(miniTaskSize);
+            dynamicServer.run();
+        } else {
+            System.err.println("Invalid server type.");
+        }
 
     }
 }
